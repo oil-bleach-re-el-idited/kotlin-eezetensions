6b3e2d29787d5d87850d727c51fae2215d445338
DeaDshot14 <mistry14darren@gmail.com>
2020-03-10 18:45:23 +0530

    v1.0.2 released

--- library/src/main/java/com/andruid/magic/eezetensions/Context.kt
+++ library/src/main/java/com/andruid/magic/eezetensions/Context.kt
@@ -5,12 +5,18 @@ package com.andruid.magic.eezetensions
 import android.Manifest
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.os.Build
+import android.os.Handler
+import android.os.Looper
+import android.provider.Settings
+import android.widget.Toast
 import androidx.annotation.ColorRes
 import androidx.annotation.DrawableRes
 import androidx.annotation.RequiresPermission
+import androidx.annotation.StringRes
 import androidx.core.content.ContextCompat
 import androidx.core.content.getSystemService
 
@@ -56,3 +62,53 @@ fun Context.hasInternet(): Boolean {
         }
     } ?: false
 }
+
+/**
+ * Extension function to get package version name for the application
+ * @return package version string
+ * @receiver context of the calling component
+ */
+fun Context.getPackageVersion(): String? {
+    return try {
+        val pInfo = packageManager.getPackageInfo(applicationContext.packageName, 0)
+        pInfo.versionName
+    } catch (e: PackageManager.NameNotFoundException) {
+        e.printStackTrace()
+        null
+    }
+}
+
+/**
+ * Extension function to show toast from background threads
+ * @param msg toast message
+ * @param duration toast duration (default = [Toast.LENGTH_SHORT]
+ * @param fromBg toast from background (default = false)
+ * @receiver context of the calling component
+ */
+fun Context.toast(msg: String, duration: Int = Toast.LENGTH_SHORT, fromBg: Boolean = false) {
+    if (fromBg) {
+        Handler(Looper.getMainLooper()).post {
+            toast(msg, duration, false)
+        }
+    } else
+        Toast.makeText(this, msg, duration).show()
+}
+
+/**
+ * Extension function to show toast from background threads
+ * @param msgRes string resource for toast message
+ * @param duration toast duration (default = [Toast.LENGTH_SHORT]
+ * @param fromBg toast from background (default = false)
+ * @receiver context of the calling component
+ */
+fun Context.toast(@StringRes msgRes: Int, duration: Int = Toast.LENGTH_SHORT, fromBg: Boolean = false) {
+    toast(getString(msgRes), duration, fromBg)
+}
+
+/**
+ * Extension function to get device name set in settings
+ * @return user set device name
+ * @receiver context of the calling component
+ */
+fun Context.getUserDeviceName(): String =
+        Settings.Global.getString(contentResolver, "device_name")
--- library/src/main/java/com/andruid/magic/eezetensions/Fragment.kt
+++ library/src/main/java/com/andruid/magic/eezetensions/Fragment.kt
@@ -3,16 +3,18 @@
 package com.andruid.magic.eezetensions
 
 import android.content.Intent
+import android.widget.Toast
 import androidx.annotation.ColorRes
 import androidx.annotation.DrawableRes
 import androidx.annotation.RequiresPermission
+import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
 
 /**
  * Extension function to load color from resource
  * @param color id of the color resource
  * @return color from color resource
- * @receiver active fragment
+ * @receiver calling fragment
  */
 fun Fragment.color(@ColorRes color: Int) = requireContext().color(color)
 
@@ -20,23 +22,41 @@ fun Fragment.color(@ColorRes color: Int) = requireContext().color(color)
  * Extension function to load drawable from resource
  * @param res id of the drawable resource
  * @return drawable from drawable resource
- * @receiver active fragment
+ * @receiver calling fragment
  */
 fun Fragment.drawable(@DrawableRes res: Int) = requireContext().drawable(res)
 
 /**
  * Extension function to start foreground service above API 26 else start background service
  * @param intent to launch the service
- * @receiver active fragment
+ * @receiver calling fragment
  */
 fun Fragment.startFgOrBgService(intent: Intent) {
     requireContext().startFgOrBgService(intent)
 }
 
 /**
- * Extension function to check whether device is connected to internet or not
- * @return true/false
- * @receiver active fragment
+ * Extension function to show toast from background threads
+ * @param msg toast message
+ * @param duration toast duration (default = [Toast.LENGTH_SHORT]
+ * @param fromBg toast from background (default = false)
+ * @receiver calling fragment
  */
-@RequiresPermission(android.Manifest.permission.ACCESS_NETWORK_STATE)
-fun Fragment.hasInternet() = requireContext().hasInternet()
+fun Fragment.toast(msg: String, duration: Int = Toast.LENGTH_SHORT, fromBg: Boolean = false) {
+    requireContext().toast(msg, duration, fromBg)
+}
+
+/**
+ * Extension function to show toast from background threads
+ * @param msgRes string resource for toast message
+ * @param duration toast duration (default = [Toast.LENGTH_SHORT]
+ * @param fromBg toast from background (default = false)
+ * @receiver calling fragment
+ */
+fun Fragment.toast(
+    @StringRes msgRes: Int,
+    duration: Int = Toast.LENGTH_SHORT,
+    fromBg: Boolean = false
+) {
+    requireContext().toast(msgRes, duration, fromBg)
+}
