ba8082b9db933a29e4fcb73f70b318395d9e4eb5
DeaDshot14 <mistry14darren@gmail.com>
2020-03-10 18:45:23 +0530

    v1.0.2 released

--- library/src/main/java/com/andruid/magic/eezetensions/Intent.kt
+++ library/src/main/java/com/andruid/magic/eezetensions/Intent.kt
@@ -8,12 +8,12 @@ import android.net.Uri
 import android.provider.Settings
 
 /**
- * Get intent to show the settings screen of the app or permissions and other settings
+ * Get intent to show the settings screen of the app for permissions and other settings
  * @return intent to launch settings page
  * @receiver context of the component
  */
 fun Context.buildSettingsIntent(): Intent {
     val uri = Uri.fromParts("package", packageName, null)
     return Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-            .setData(uri)
+        .setData(uri)
 }
